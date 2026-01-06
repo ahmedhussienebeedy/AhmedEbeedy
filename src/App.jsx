d1948f95cf0560b4fc60be12a33b9289d0980eb2
ahmedhussienebeedy <127639140+ahmedhussienebeedy@users.noreply.github.com>
2026-01-06 11:12:16 +0200

    Update project for Vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,15 @@ import Notfound from "./components/Notfound/Notfound";
 import Layout from "./components/Layout/Layout";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useState } from "react";
 
 function App() {
-  const [theme, Settheme] = useState(localStorage.getItem("theme") || "light");
 
-  function toggle() {
-    if (theme === "light") {
-      Settheme("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      Settheme("light");
-      localStorage.setItem("theme", "light");
-    }
-  }
+
 
   const routing = createBrowserRouter([
     {
-      element: <Layout theme={theme} toggle={toggle} />,
+      element: <Layout  />,
       children: [
         { index: true, element: <Home /> }, // Home
         { path: "contact", element: <Contact /> },
