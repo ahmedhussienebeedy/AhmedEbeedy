c9967b6e2af9a5da2027ab55e6467f9ef6990d95
ahmedhussienebeedy <127639140+ahmedhussienebeedy@users.noreply.github.com>
2026-03-19 07:56:10 +0200

    fix routing for vercel

--- vite-template/vite.config.ts
+++ vite-template/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // 👈 مهم جداً
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
